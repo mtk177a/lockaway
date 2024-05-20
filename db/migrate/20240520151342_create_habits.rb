29ee01cb7f132482c557c517ff6695da793b3918
mtk177a <mtk177a@gmail.com>
2024-05-21 00:22:18 +0900

    マイグレーションファイルの修正

--- db/migrate/20240520151342_create_habits.rb
+++ db/migrate/20240520151342_create_habits.rb
@@ -3,7 +3,7 @@ class CreateHabits < ActiveRecord::Migration[7.1]
     create_table :habits do |t|
       t.references :user, null: false, foreign_key: true
       t.string :name, null: false
-      t.string :habit_type, null: false
+      t.integer :habit_type, null: false
       t.text :description
       t.datetime :start_date, null: false, default: -> { 'CURRENT_TIMESTAMP' }
 
