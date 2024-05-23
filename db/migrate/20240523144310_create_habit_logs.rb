5ace78ebc76374348c326e9bad3cb8ef528e2724
mtk177a <mtk177a@gmail.com>
2024-05-23 23:46:48 +0900

    マイグレーションファイルの修正

--- db/migrate/20240523144310_create_habit_logs.rb
+++ db/migrate/20240523144310_create_habit_logs.rb
@@ -2,10 +2,12 @@ class CreateHabitLogs < ActiveRecord::Migration[7.1]
   def change
     create_table :habit_logs do |t|
       t.references :habit, null: false, foreign_key: true
-      t.date :date
-      t.integer :status
+      t.date :date, null: false
+      t.integer :status, null: false, default: 0
 
       t.timestamps
     end
+
+    add_index :habit_logs, [:habit_id, :date], unique: true
   end
 end
