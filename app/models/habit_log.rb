1c6e8774969be94d5442ad958330fad7fc4e9bf1
mtk177a <mtk177a@gmail.com>
2024-05-27 23:30:17 +0900

    HabitLogモデルの修正

--- app/models/habit_log.rb
+++ app/models/habit_log.rb
@@ -6,4 +6,11 @@ class HabitLog < ApplicationRecord
   validates :date, presence: true
   validates :status, presence: true
   validates :habit_id, uniqueness: { scope: :date, message: "has already been logged for this date" }
+  validate :date_cannot_be_before_habit_start
+
+  def date_cannot_be_before_habit_start
+    if date.present? && date < habit.start_date
+      errors.add(:date, "can't be before the habit start date")
+    end
+  end
 end
