b7538b596b6ae7aa6eb2cd8b5414f9ef44f7d655
mtk177a <mtk177a@gmail.com>
2024-05-29 01:17:12 +0900

    Habitモデルに初期ログ生成機能を追加

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -2,9 +2,19 @@ class Habit < ApplicationRecord
   belongs_to :user
   has_many :habit_logs, dependent: :destroy
 
+  after_create :generate_initial_logs
+
   enum habit_type: { good: 0, bad: 1 }
 
   validates :name, presence: true
   validates :habit_type, presence: true
   validates :start_date, presence: true
+
+  private
+
+  def generate_initial_logs
+    (start_date..Date.today).each do |date|
+      habit_logs.find_or_create_by(date: date)
+    end
+  end
 end
