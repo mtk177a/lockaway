045645b2adcf64147f0f67eba7fe22ac25f75ca3
mtk177a <mtk177a@gmail.com>
2024-06-06 23:44:31 +0900

    マイグレーションファイルの修正

--- db/migrate/20240605144938_create_rewards.rb
+++ db/migrate/20240605144938_create_rewards.rb
@@ -1,10 +1,10 @@
 class CreateRewards < ActiveRecord::Migration[7.1]
   def change
     create_table :rewards do |t|
-      t.string :name
+      t.string :name,            null: false
       t.text :description
-      t.string :condition_type
-      t.integer :threshold
+      t.string :condition_type,  null: false
+      t.integer :threshold,      null: false
 
       t.timestamps
     end
--- db/migrate/20240605145117_create_habit_rewards.rb
+++ db/migrate/20240605145117_create_habit_rewards.rb
@@ -1,7 +1,7 @@
 class CreateHabitRewards < ActiveRecord::Migration[7.1]
   def change
     create_table :habit_rewards do |t|
-      t.references :habit, null: false, foreign_key: true
+      t.references :habit,  null: false, foreign_key: true
       t.references :reward, null: false, foreign_key: true
 
       t.timestamps
