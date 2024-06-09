0cbb19e2686dd4a035d4103cd575233acd92e220
mtk177a <mtk177a@gmail.com>
2024-06-10 01:10:38 +0900

    `Rewards`コントローラに`index`, `show`, `user_rewards`アクションを追加

--- app/controllers/rewards_controller.rb
+++ app/controllers/rewards_controller.rb
@@ -1,7 +1,13 @@
 class RewardsController < ApplicationController
   def index
+    @rewards = Reward.all
   end
 
   def show
+    @reward = Reward.find(params[:id])
+  end
+
+  def user_rewards
+    @user_rewards = current_user.rewards
   end
 end
