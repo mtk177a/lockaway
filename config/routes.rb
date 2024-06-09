39e3fd33c170caa0cfaf11ba57d2a691d3f358e7
mtk177a <mtk177a@gmail.com>
2024-06-09 17:10:04 +0900

    未記録の習慣ログ一覧のルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
-  get 'unlogged_habit_logs/index'
   root 'habits#index'
 
   resources :habits do
     resources :habit_logs, only: [:new, :create, :index, :update]
   end
+
   resources :users
 
   get 'login' => 'user_sessions#new', :as => :login
   post 'login' => "user_sessions#create"
   delete 'logout' => 'user_sessions#destroy', :as => :logout
+
+  get 'unlogged_habit_logs', to: 'unlogged_habit_logs#index', as: 'unlogged_habit_logs'
 end
