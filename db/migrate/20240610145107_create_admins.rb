5796ba6b43d8df5d295efb0d7cec6452bad40917
mtk177a <mtk177a@gmail.com>
2024-06-10 23:59:31 +0900

    マイグレーションファイルの修正

--- db/migrate/20240610145107_create_admins.rb
+++ db/migrate/20240610145107_create_admins.rb
@@ -1,11 +1,11 @@
 class CreateAdmins < ActiveRecord::Migration[7.1]
   def change
     create_table :admins do |t|
-      t.string :email
+      t.string :email,            null: false, index: { unique: true }
       t.string :crypted_password
       t.string :salt
 
-      t.timestamps
+      t.timestamps                null: false
     end
   end
 end
