b1648430c683df0e48c2a31e029480de93612a74
mtk177a <mtk177a@gmail.com>
2024-06-12 00:07:23 +0900

    報酬のCRUD機能の実装

--- app/controllers/admin/rewards_controller.rb
+++ app/controllers/admin/rewards_controller.rb
@@ -1,2 +1,49 @@
-class Admin::RewardsController < ApplicationController
+class Admin::RewardsController < Admin::BaseController
+  before_action :set_reward, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @rewards = Reward.all
+  end
+
+  def show
+  end
+
+  def new
+    @reward = Reward.new
+  end
+
+  def create
+    @reward = Reward.new(reward_params)
+    if @reward.save
+      redirect_to admin_rewards_path, notice: 'Reward was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @reward.update(reward_params)
+      redirect_to admin_rewards_path, notice: 'Reward was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @reward.destroy
+    redirect_to admin_rewards_path, notice: 'Reward was successfully deleted.'
+  end
+
+  private
+
+  def set_reward
+    @reward = Reward.find(params[:id])
+  end
+
+  def reward_params
+    params.require(:reward).permit(:name, :description, :condition_type, :threshold)
+  end
 end
