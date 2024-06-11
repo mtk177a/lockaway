b5a142c77e0ed14f665658c8f0ac6e6f37eead52
mtk177a <mtk177a@gmail.com>
2024-06-12 00:56:39 +0900

    ユーザー管理コントローラーの設定

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,2 +1,21 @@
-class Admin::UsersController < ApplicationController
+class Admin::UsersController < Admin::BaseController
+  before_action :set_user, only: [:show, :destroy]
+
+  def index
+    @users = User.all
+  end
+
+  def show
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to admin_users_path, notice: 'User was successfully deleted.'
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
