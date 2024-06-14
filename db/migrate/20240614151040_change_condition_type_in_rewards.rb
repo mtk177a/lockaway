4bcebf07331ce11ef1aea7fdcb9c1648989e2c7a
mtk177a <mtk177a@gmail.com>
2024-06-15 00:17:28 +0900

    マイグレーションファイルの再修正

--- db/migrate/20240614151040_change_condition_type_in_rewards.rb
+++ db/migrate/20240614151040_change_condition_type_in_rewards.rb
@@ -1,15 +1,6 @@
 class ChangeConditionTypeInRewards < ActiveRecord::Migration[7.1]
-  def up
-    change_column :rewards, :condition_type, :integer, using: 'condition_type::integer'
-
-    Reward.where(condition_type: 'continuous_days').update_all(condition_type: 0)
-    Reward.where(condition_type: 'total_days').update_all(condition_type: 1)
-  end
-
-  def down
-    change_column :rewards, :condition_type, :string
-
-    Reward.where(condition_type: 0).update_all(condition_type: 'continuous_days')
-    Reward.where(condition_type: 1).update_all(condition_type: 'total_days')
+  def change
+    remove_column :rewards, :condition_type, :string
+    add_column :rewards, :condition_type, :integer, null: false
   end
 end
