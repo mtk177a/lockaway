78591ef3b3281444a6d8ec1756fa6d69f41b9026
mtk177a <mtk177a@gmail.com>
2024-06-16 23:43:06 +0900

    `PublicRewards`コントローラの`index`アクションの実装

--- app/controllers/public_rewards_controller.rb
+++ app/controllers/public_rewards_controller.rb
@@ -1,4 +1,7 @@
 class PublicRewardsController < ApplicationController
   def index
+    @habit_rewards = HabitReward.includes(:habit, :reward, habit: :user).order(created_at: :desc).page(params[:page])
+    @habit_rewards = @habit_rewards.where(habits: { habit_type: params[:habit_type] }) if params[:habit_type].present?
+    @habit_rewards = @habit_rewards.joins(habit: :user).where(users: { username: params[:username] }) if params[:username].present?
   end
 end
