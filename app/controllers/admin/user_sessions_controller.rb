0ca7c1ab5d4d55c717353d84e96dbb7b634a240c
mtk177a <mtk177a@gmail.com>
2024-06-17 02:22:05 +0900

    管理者用のセッションコントローラのアクション実装

--- app/controllers/admin/user_sessions_controller.rb
+++ app/controllers/admin/user_sessions_controller.rb
@@ -1,10 +1,24 @@
 class Admin::UserSessionsController < ApplicationController
+  skip_before_action :require_login, only: [:new, :create]
+
   def new
   end
 
   def create
+    user = login(params[:email], params[:password])
+    if user&.admin?
+      session[:user_id] = user.id
+      redirect_back_or_to(admin_dashboard_path, notice: 'Login successful')
+    else
+      logout if user
+      flash.now[:alert] = 'Login failed'
+      render :new
+    end
   end
 
   def destroy
+    logout
+    session[:user_id] = nil
+    redirect_to(admin_login_path, notice: 'Logged out!')
   end
 end
