3c00529d270c44027f305eed9b74e2f8868dde2f
mtk177a <mtk177a@gmail.com>
2024-06-17 02:26:51 +0900

    ルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
   root 'habits#index'
 
+  get 'login', to: 'user_sessions#new', as: :login
+  post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy', as: :logout
+
   resources :habits do
     resources :habit_logs, only: [:new, :create, :index, :update]
   end
+
+  resources :public_habits, only: [:index]
+
+  resources :public_rewards, only: [:index]
 
   resources :rewards, only: [:index, :show] do
     collection do
@@ -12,23 +20,15 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :users
-
-  resources :public_habits, only: [:index]
-
-  resources :public_rewards, only: [:index]
-
   resources :unlogged_habit_logs, only: [:index]
 
-  get 'admin_login' => 'admin_sessions#new', :as => :admin_login
-  post 'admin_login' => "admin_sessions#create"
-  delete 'admin_logout' => 'admin_sessions#destroy', :as => :admin_logout
-
-  get 'login' => 'user_sessions#new', :as => :login
-  post 'login' => "user_sessions#create"
-  delete 'logout' => 'user_sessions#destroy', :as => :logout
+  resources :users
 
   namespace :admin do
+    get 'login', to: 'user_sessions#new', as: :login
+    post 'login', to: 'user_sessions#create'
+    delete 'logout', to: 'user_sessions#destroy', as: :logout
+
     get 'dashboard', to: 'dashboard#index'
     resources :habits, only: [:index, :show, :destroy]
     resources :rewards
