afdab20f09999d32a5b864ab8851b27bbf03a25b
mtk177a <mtk177a@gmail.com>
2024-06-18 00:12:28 +0900

    `Reward`モデルに`habit_type`を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_16_170209) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_17_151035) do
   create_table "admins", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email", null: false
     t.string "crypted_password"
@@ -62,6 +62,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_16_170209) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "condition_type", null: false
+    t.integer "habit_type"
   end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
