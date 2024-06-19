224723728efacdc0b41b34ce54b37aa6fd39cd90
mtk177a <mtk177a@gmail.com>
2024-06-20 00:39:48 +0900

    未記録の習慣ログ一覧に表示される習慣ログの修正

--- app/controllers/unlogged_habit_logs_controller.rb
+++ app/controllers/unlogged_habit_logs_controller.rb
@@ -1,6 +1,6 @@
 class UnloggedHabitLogsController < ApplicationController
   def index
     @unlogged_habit_logs = HabitLog.joins(:habit)
-                                   .where(habits: { user_id: current_user.id }, date: Date.today, status: nil)
+                                   .where(habits: { user_id: current_user.id }, status: nil)
   end
 end
