700bc86cd8db0c82a0fa25fd720b88a11bc6cc40
mtk177a <mtk177a@gmail.com>
2024-06-24 23:48:25 +0900

    `public_habits#show`の追加

--- app/controllers/public_habits_controller.rb
+++ app/controllers/public_habits_controller.rb
@@ -1,5 +1,5 @@
 class PublicHabitsController < ApplicationController
-  skip_before_action :require_login, only: [:index]
+  skip_before_action :require_login, only: [:index, :show]
 
   def index
     @habits = Habit.public_habits
@@ -7,5 +7,9 @@ class PublicHabitsController < ApplicationController
     @habits = @habits.order(created_at: :asc) if params[:sort] == 'oldest'
     @habits = @habits.where(habit_type: params[:habit_type]) if params[:habit_type].present?
     @habits = @habits.page(params[:page])
+  end
+
+  def show
+    @habit = Habit.public_habits.find(params[:id])
   end
 end
