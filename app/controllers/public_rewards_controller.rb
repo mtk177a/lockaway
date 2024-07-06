ef57521cab3eba2a63d3e90c8649adefb15dfc6c
mtk177a <mtk177a@gmail.com>
2024-07-06 23:20:19 +0900

    `public_rewards#index`にRansackを使用したソートと検索機能のロジックを追加

--- app/controllers/public_rewards_controller.rb
+++ app/controllers/public_rewards_controller.rb
@@ -2,8 +2,9 @@ class PublicRewardsController < ApplicationController
   skip_before_action :require_login, only: [:index]
 
   def index
-    @habit_rewards = HabitReward.includes(:habit, :reward, habit: :user).order(created_at: :desc).page(params[:page])
-    @habit_rewards = @habit_rewards.where(habits: { habit_type: params[:habit_type] }) if params[:habit_type].present?
-    @habit_rewards = @habit_rewards.joins(habit: :user).where(users: { username: params[:username] }) if params[:username].present?
+    @q = HabitReward.joins(habit: :user)
+                    .where(habits: { public: true })
+                    .ransack(params[:q])
+    @habit_rewards = @q.result.includes(:habit, :reward).order(created_at: :desc).page(params[:page])
   end
 end
