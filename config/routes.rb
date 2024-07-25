1809e09f785e57265ab36dfeeeeeb1dcfe7de1c5
mtk177a <mtk177a@gmail.com>
2024-07-26 02:00:30 +0900

    ルートパスを`home#index`に設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'home/index'
-  root 'public_habits#index'
+  root 'home#index'
 
   get 'login', to: 'user_sessions#new', as: :login
   post 'login', to: 'user_sessions#create'
