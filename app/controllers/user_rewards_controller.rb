c099e2f4be4ac70b02d32e4dffee05b792497348
mtk177a <mtk177a@gmail.com>
2024-07-30 01:04:57 +0900

    `user_rewards#show`アクションの実装

--- app/controllers/user_rewards_controller.rb
+++ app/controllers/user_rewards_controller.rb
@@ -5,4 +5,12 @@ class UserRewardsController < ApplicationController
                     .ransack(params[:q])
     @user_rewards = @q.result.order(created_at: :desc).page(params[:page])
   end
+
+  def show
+    @habit_reward = HabitReward.find(params[:id])
+    respond_to do |format|
+      format.html
+      format.turbo_stream { render partial: 'shared/reward_modal', locals: { reward: @habit_reward.reward } }
+    end
+  end
 end
