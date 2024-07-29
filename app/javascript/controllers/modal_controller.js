4c1d965a5745f30cbba3efec4d3a27d1acc2f457
mtk177a <mtk177a@gmail.com>
2024-07-30 01:12:42 +0900

    モーダル用のStimulusコントローラを修正

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -3,5 +3,14 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="modal"
 export default class extends Controller {
   connect() {
+    this.showModal()
+  }
+
+  showModal() {
+    this.element.showModal()
+  }
+
+  close() {
+    this.element.close()
   }
 }
