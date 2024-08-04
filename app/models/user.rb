8d68b5a786412a3888e69920bf78787956ea0dab
mtk177a <mtk177a@gmail.com>
2024-08-05 01:27:07 +0900

    `Authentication`モデルと`User`モデルの関連付け

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
 
+  has_many :authentications, :dependent => :destroy
+  accepts_nested_attributes_for :authentications
+
   enum role: { general: 0, admin: 1 }
 
   has_many :habits, dependent: :destroy
