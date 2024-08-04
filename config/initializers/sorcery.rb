a85b5019b1daf650c615367eeaa938a9e37594fd
mtk177a <mtk177a@gmail.com>
2024-08-05 02:36:22 +0900

    開発環境と本番環境で別の`callback_url`を設定

--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -167,7 +167,7 @@ Rails.application.config.sorcery.configure do |config|
   #
     config.google.key = Rails.application.credentials.dig(:google, :google_client_id)
     config.google.secret = Rails.application.credentials.dig(:google, :google_client_secret)
-    config.google.callback_url = 'http://localhost:3000/oauth/callback?provider=google'
+    config.google.callback_url = Settings.sorcery[:google_callback_url]
     config.google.user_info_mapping = {:email => "email", :username => "name"}
   # config.google.scope = "https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile"
   end
