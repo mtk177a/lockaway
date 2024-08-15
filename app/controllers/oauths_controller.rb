d36a9dd912a5ad1f98cf5271a27c55a20f183926
mtk177a <mtk177a@gmail.com>
2024-08-15 20:54:11 +0900

    Googleログインの処理を改善

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -2,23 +2,49 @@ class OauthsController < ApplicationController
   skip_before_action :require_login
 
   def oauth
-    #指定されたプロバイダの認証ページにユーザーをリダイレクトさせる
+    # 指定されたプロバイダの認証ページにユーザーをリダイレクトさせる
     login_at(auth_params[:provider])
   end
 
   def callback
     provider = auth_params[:provider]
-    # 既存のユーザーをプロバイダ情報を元に検索し、存在すればログイン
-    if (@user = login_from(provider))
-      redirect_to root_path, notice:"#{provider.titleize}アカウントでログインしました"
-    else
-      begin
-        # ユーザーが存在しない場合はプロバイダ情報を元に新規ユーザーを作成し、ログイン
-        signup_and_login(provider)
-        redirect_to root_path, notice:"#{provider.titleize}アカウントでログインしました"
-      rescue
-        redirect_to root_path, alert:"#{provider.titleize}アカウントでのログインに失敗しました"
+    notice_message = "#{provider.titleize}アカウントでログインしました"
+
+    begin
+      # ログインを試みる
+      @user = login_from(provider)
+
+      unless @user
+        # プロバイダ情報の取得
+        sorcery_fetch_user_hash(provider)
+
+        # 既存のユーザーを探す
+        @user = User.find_by(email: @user_hash.dig(:user_info, 'email'))
+
+        if @user
+          # 既存のユーザーにプロバイダ情報を追加
+          @user.add_provider_to_user(provider, @user_hash[:uid].to_s)
+          notice_message = "#{provider.titleize}アカウントの情報を追加してログインしました！"
+        else
+          # 新しいユーザーを作成
+          @user = create_from(provider)
+          notice_message = "#{provider.titleize}アカウントでログインしました！ユーザー名を設定してください！"
+          after_login_path = user_path(@user)
+        end
+
+        reset_session
+        auto_login(@user)
       end
+
+      redirect_to after_login_path || root_path, success: notice_message
+    rescue OAuth::Unauthorized, OAuth::Error => e
+      # OAuth関連エラーの処理
+      Rails.logger.error("OAuth error: #{e.message}")
+      redirect_to login_path, alert: "#{provider.titleize}アカウントでのログインに失敗しました。もう一度お試しください。"
+    rescue StandardError => e
+      # その他のエラー処理
+      Rails.logger.error("Unexpected error during OAuth callback: #{e.message}")
+      redirect_to login_path, alert: "予期しないエラーが発生しました。もう一度お試しください。"
     end
   end
 
@@ -26,11 +52,5 @@ class OauthsController < ApplicationController
 
   def auth_params
     params.permit(:code, :provider)
-  end
-
-  def signup_and_login(provider)
-    @user = create_from(provider)
-    reset_session
-    auto_login(@user)
   end
 end
