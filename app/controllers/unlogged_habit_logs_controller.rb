37c83a7e366bfdcb7b29081f5ed059b3407cc942
mtk177a <mtk177a@gmail.com>
2024-08-18 00:29:30 +0900

    習慣の新しい順に並べ替え

--- app/controllers/unlogged_habit_logs_controller.rb
+++ app/controllers/unlogged_habit_logs_controller.rb
@@ -2,5 +2,6 @@ class UnloggedHabitLogsController < ApplicationController
   def index
     @unlogged_habit_logs = HabitLog.joins(:habit)
                                    .where(habits: { user_id: current_user.id }, status: nil)
+                                   .order('habits.created_at DESC, date DESC')
   end
 end
