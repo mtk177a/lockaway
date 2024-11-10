d488b6e3b0b68b8dc627a075ba6e4a39688f88b6
mtk177a <mtk177a@gmail.com>
2024-11-10 17:15:02 +0900

    Capybaraの`selenium_remote_chrome`ドライバー設定

---
+++ spec/support/capybara.rb
@@ -0,0 +1,18 @@
+require 'selenium-webdriver'
+
+Capybara.register_driver :selenium_remote_chrome do |app|
+  options = Selenium::WebDriver::Chrome::Options.new
+  options.add_argument('--headless')
+  options.add_argument('--no-sandbox')
+  options.add_argument('--disable-dev-shm-usage')
+  options.add_argument('--disable-gpu')
+
+  Capybara::Selenium::Driver.new(
+    app,
+    browser: :remote,
+    url: ENV.fetch("SELENIUM_DRIVER_URL", "http://chrome:4444/wd/hub"),
+    capabilities: options
+  )
+end
+
+Capybara.default_driver = :selenium_remote_chrome
