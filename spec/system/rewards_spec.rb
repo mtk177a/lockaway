7d8356d9c8a3ef318559300140d48c81065f8f6f
mtk177a <mtk177a@gmail.com>
2024-11-13 00:24:02 +0900

    モーダル表示の待機時間を調整

--- spec/system/rewards_spec.rb
+++ spec/system/rewards_spec.rb
@@ -26,7 +26,7 @@ RSpec.describe '報酬機能', type: :system do
       click_button '達成した'
     end
 
-    expect(page).to have_selector("dialog#my_modal", visible: true, wait: 30) # モーダルが表示されるのを待機
+    expect(page).to have_selector("dialog#my_modal", visible: true, wait: 10) # モーダルが表示されるのを待機
     expect(page).to have_content('報酬を獲得しました！')
     expect(page).to have_content(reward.name)
   end
