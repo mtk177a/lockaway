08c6eb2fd9dfc43c6491ab0663887e75a99b6f24
mtk177a <mtk177a@gmail.com>
2024-11-13 18:48:26 +0900

    報酬機能のシステムスペックを修正

--- spec/system/rewards_spec.rb
+++ spec/system/rewards_spec.rb
@@ -14,4 +14,19 @@ RSpec.describe '報酬機能', type: :system do
 
     expect(page).to have_content('1日達成')
   end
+
+  it '条件達成により報酬を獲得できること', js: true do
+    habit = create(:habit, user: user, name: '今日の運動', start_date: Date.today)
+    reward = create(:reward, name: '1日達成', threshold: 1)
+
+    visit habit_path(habit)
+
+    expect(page).to have_css("#habit_#{habit.id}", wait: 5)
+    within("#habit_#{habit.id}") do
+      click_button '達成した'
+    end
+
+    visit user_rewards_path(user)
+    expect(page).to have_content(reward.name, wait: 5)
+  end
 end
