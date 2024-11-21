914a5feabf77c1aec0827fbaadbd610aed3356cd
mtk177a <mtk177a@gmail.com>
2024-11-22 00:14:39 +0900

    `PublicReward#index`のリストが非同期で更新されるように修正

--- app/controllers/public_rewards_controller.rb
+++ app/controllers/public_rewards_controller.rb
@@ -6,5 +6,15 @@ class PublicRewardsController < ApplicationController
                     .where(habits: { public: true })
                     .ransack(params[:q])
     @habit_rewards = @q.result.includes(:habit, :reward).order(created_at: :desc).page(params[:page])
+
+    respond_to do |format|
+      format.html # 通常のHTMLレンダリング
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.replace(
+          'reward_list', # Turbo Streamで置き換える部分のID
+          partial: 'shared/reward_list', locals: { rewards: @habit_rewards }
+        )
+      end
+    end
   end
 end
