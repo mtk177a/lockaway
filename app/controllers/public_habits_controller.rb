ff655d35f268db2732ba2aaa5ef9bc83a53b46f0
mtk177a <mtk177a@gmail.com>
2024-11-29 01:14:10 +0900

    `public_habits#search`を`skip_before_action`に設定

--- app/controllers/public_habits_controller.rb
+++ app/controllers/public_habits_controller.rb
@@ -1,5 +1,5 @@
 class PublicHabitsController < ApplicationController
-  skip_before_action :require_login, only: [:index, :show]
+  skip_before_action :require_login, only: [:index, :show, :search]
 
   def index
     @q = Habit.public_habits.ransack(params[:q])
